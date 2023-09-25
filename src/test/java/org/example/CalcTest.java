9d0a81768a32b3c134b101ba32023ea0f5e9c797
Pettina <1353023196@qq.com>
2023-09-25 12:14:40 +0800

    fixed the bug

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -6,23 +6,14 @@ import org.junit.jupiter.api.Test;
 public class CalcTest {
   Calc c = new Calc();
 
-    @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
-    }
-@Test
-    public void testSubtraction() {
-        // 定义测试数据和预期结果
-        int a = 4;
-        int b = 2;
-        int expected = 2;
+  @Test
+  void testAddition() {
+    assertEquals(4, c.add(2, 2));
+  }
 
-        // 调用你的代码进行减法操作
-        Calculator calculator = new Calculator();
-        int actual = calculator.subtract(a, b);
-
-        // 使用断言来验证实际结果是否与预期结果一致
-        assertEquals(expected, actual);
-    }
+  @Test
+  void testsubtract() {
+    assertEquals(2, c.subtract(4, 2));
+  }
 
 }
